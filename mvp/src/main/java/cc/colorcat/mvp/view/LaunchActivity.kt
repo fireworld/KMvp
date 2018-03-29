02d62a5b56c2b167768fa93bffa23f9aa8f8aacd
cxx <xx.ch@outlook.com>
2018-03-29 16:30:35 +0800

    1. upgrade gradle

--- mvp/src/main/java/cc/colorcat/mvp/view/LaunchActivity.kt
+++ mvp/src/main/java/cc/colorcat/mvp/view/LaunchActivity.kt
@@ -18,19 +18,19 @@ class LaunchActivity : BaseActivity() {
         setContentView(R.layout.activity_launch)
 
         btn_show_courses.setOnClickListener(mClick)
-        btn_show_courses_with_fragment.setOnClickListener(mClick)
+        btn_show_courses_by_fragment.setOnClickListener(mClick)
         btn_request_permission.setOnClickListener(mClick)
     }
 
     private val mClick = View.OnClickListener {
-        when {
-            it.id == R.id.btn_show_courses -> {
+        when (it.id) {
+            R.id.btn_show_courses -> {
                 navigateTo(CoursesActivity::class.java)
             }
-            it.id == R.id.btn_show_courses_with_fragment -> {
+            R.id.btn_show_courses_by_fragment -> {
                 startActivity(newIntent(this@LaunchActivity, ContainerActivity::class.java, Const.key.container_layout to R.layout.activity_container_courses))
             }
-            it.id == R.id.btn_request_permission -> {
+            R.id.btn_request_permission -> {
                 requestPermissions(arrayOf(Manifest.permission.READ_PHONE_STATE), object : PermissionListener {
                     override fun onAllGranted() {
                         toast("All permission granted")
